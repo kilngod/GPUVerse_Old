0d327018319c6d199342debab51dd45c8612f477
kilngod <kilngod@outlook.com>
2024-01-09 09:01:09 -0700

    updates

---
+++ MauiWorkingCheck/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace MauiWorkingCheck;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+    protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
